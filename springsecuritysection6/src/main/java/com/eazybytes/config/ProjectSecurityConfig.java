4263022de4c09c80fff4545291037c83c27a7666
zzjiho <youngyoungnim@gmail.com>
2023-12-23 00:40:24 +0900

    no message

--- springsecuritysection6/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritysection6/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.eazybytes.config;
 
+import com.eazybytes.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -26,7 +31,13 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception { //http요청에 대한 보안을 설정, 페이지권한설정,로그인페이지,로그아웃 메소드 등에 대한 설정 작성
 
-        http.cors().configurationSource(new CorsConfigurationSource() { //cors 설정
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+
+        http.securityContext().requireExplicitSave(false)//securityContext를 세션에 저장하지 않도록 설정
+                .and().sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)). //첫 로그인에 항상 JSESSIONID를 생성
+                cors().configurationSource(new CorsConfigurationSource() { //cors 설정
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
@@ -37,8 +48,9 @@ public class ProjectSecurityConfig {
                         config.setMaxAge(3600L);
                         return config;
                     }
-                }).and()
-
+                }).and().csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact", "/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())).
+                addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests()
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated() //인증된 사용자만 접근 가능
                 .requestMatchers("/notices", "/contact", "/register", "/error").permitAll() //모든 사용자 접근 가능
--- springsecuritysection6/src/main/java/com/eazybytes/controller/ContactController.java
+++ springsecuritysection6/src/main/java/com/eazybytes/controller/ContactController.java
@@ -4,6 +4,7 @@ import com.eazybytes.model.Contact;
 import com.eazybytes.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class ContactController {
     private ContactRepository contactRepository;
 
 
-    @GetMapping("/contact")
+    @PostMapping("/contact")
     public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
